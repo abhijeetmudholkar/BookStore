f4197e9ab68e0b79191b7b15a496220be6b4be22
abhijeetmudholkar <126692571+abhijeetmudholkar@users.noreply.github.com>
2023-09-20 14:53:54 +0530

    changes added

---
+++ BookStore/BookStore/Controllers/BookController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "Get All Books";
+        }
+        public string BookDetails(int id)
+        {
+            return $"Book Details of id : {id}";
+        }
+        public string SearchBook(string BookName,string writer)
+        {
+            return $"Book Name is:{BookName} and writer is :{writer}";
+        }
+    }
+}
